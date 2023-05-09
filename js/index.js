0adfaf44b0fc3847e056ea718dfb0986a8a58256
Javier Ruiz Hoyos <jav.ruiz200@gmail.com>
2023-05-09 01:36:39 -0500

    pagina

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function darSi() {
+    alert("Elegiste bien");
+
+    setTimeout(() => window.open('https://youtu.be/BFlG5sD540k'), 500);
+}
+
+function darNo() {
+    alert("Elegiste el camino del mal");
+    setTimeout(() => window.open('https://www.youtube.com/watch?v=A9uFQa3Fv6c'), 500);
+}
