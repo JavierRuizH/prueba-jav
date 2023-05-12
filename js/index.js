feead5bc9bd8e93eb2f3a4b10ac3b58cee39ba51
Javier Ruiz Hoyos <jav.ruiz200@gmail.com>
2023-05-12 17:02:18 -0500

    Cambio de nombre .html, css y links del js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 function darSi() {
-    alert("Elegiste bien");
+    alert("Conectate puessssss");
 
     setTimeout(() => window.open('https://youtu.be/BFlG5sD540k'), 500);
 }
 
 function darNo() {
     alert("Elegiste el camino del mal");
-    setTimeout(() => window.open('https://www.youtube.com/watch?v=A9uFQa3Fv6c'), 500);
+    setTimeout(() => window.open('https://www.youtube.com/watch?v=5PlqjVCrH3M'), 500);
 }
